10bccdfb0604a9277ef8f2db57bbe1d1b2d61ee9
Prachi-Barnwal <70744266+Prachi-Barnwal@users.noreply.github.com>
2020-10-15 22:50:15 +0530

    JAVA Assignment of Day 2 Eclipse

---
+++ Day_2/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		employee e1= new employee();
+		e1.name = "Roma";
+		e1.age = 20;
+		e1.city = "Asansol";
+		e1.details();
+		
+		employee e2= new employee();
+		e2.name = "Rahul";
+		e2.age = 21;
+		e2.city = "Jaipur";
+		e2.details();
+		
+	}
+
+}
---
+++ Day_2/employee.java
@@ -0,0 +1,18 @@
+
+public class employee {
+	
+	String name, city;
+	int age;
+
+	
+	public void details()
+	{
+	
+		System.out.println("Details are:-");
+		System.out.println("The Name is "+ name);
+		System.out.println("The age is "+ age);
+		System.out.println("The City is "+ city);
+
+	}
+
+}
